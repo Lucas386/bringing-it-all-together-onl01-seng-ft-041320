5e3c08eb1ffd3ea1e964241f58f5a6861d2d808c
Lucas Easley <lucaseasley11@gmail.com>
2020-05-14 18:05:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  attr_accessor id: nil, :name, :breed
+  attr_accessor id: , :name, :breed
 
   def initialize(attributes)
     #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
-    # self.id ||= nil
+    self.id ||= nil
   end
 
   def self.create_table
